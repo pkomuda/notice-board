58c3a1d6b76bb6010b415ffbbbd59a58dd6c9bd8
Jakub Bogdan <kubab1998@o2.pl>
2020-05-08 17:15:27 +0200

    Notifications moved to another file

--- src/main/js/src/components/NavigationBar.js
+++ src/main/js/src/components/NavigationBar.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 import "./NavigationBar.css";
+import swal from 'sweetalert';
 
 class NavigationBar extends React.Component {
 
     render() {
+        if(!navigator.onLine) {
+            swal({
+                title: "You are offline",
+                text: "Displayed data may be out of date",
+                icon: "warning",
+                closeOnClickOutside: true
+            });
+        }
         return (
             <Navbar expand="lg">
                 <Navbar.Brand>Notice board</Navbar.Brand>
--- src/main/js/src/serviceWorker.js
+++ src/main/js/src/serviceWorker.js
@@ -10,8 +10,6 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-import swal from 'sweetalert'
-
 const isLocalhost = Boolean(
   window.location.hostname === "localhost" ||
     // [::1] is the IPv6 localhost address.
@@ -126,13 +124,6 @@ function checkValidServiceWorker(swUrl, config) {
       console.log(
         "No internet connection found. App is running in offline mode."
       );
-
-      swal({
-        title: "You are offline",
-        text: "Displayed data may be out of date",
-        icon: "warning",
-        closeOnClickOutside: true
-      });
     });
 }
 
